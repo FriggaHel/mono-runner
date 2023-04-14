4fec81afe0002b73ec85b57761b8e717fab17ffd
Felix P <felix@saucelabs.com>
2023-04-14 16:06:56 +0200

    Init project

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+
+async function main () {
+  console.log(`mono-runner`);
+}
+
+(async function () {
+  await main();
+})();
---
+++ tests/main.spec.ts
@@ -0,0 +1,5 @@
+describe('main', function () {
+  test('default', function () {
+    expect(true).toBe(true);
+  });
+});
